c8cf16bbcec59ff1176d3018057ee8d56dc2c31a
Irina Kurus <evans.mira@yandex.ru>
2023-06-15 03:01:46 +0300

    fix

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,6 +2,8 @@ from brain_games.cli import welcome_user
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    user_name = welcome_user()
-    return user_name
+    welcome_user()
+
+
+if __name__ == '__main__':
+    main()
