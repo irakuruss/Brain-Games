5931f31b49cf08be410be97b9c1c738092f5d9ca
irakuruss <132392842+irakuruss@users.noreply.github.com>
2023-06-20 13:15:06 +0300

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,18 +4,18 @@ from random import randint
 GAME_RULES = 'What is the result of the expression?'
 
 
-def determine_num_and_answer():
+def determine_task_and_answer():
     START_NUM = 1
     LAST_NUM = 100
     num1 = randint(START_NUM, LAST_NUM)
     num2 = randint(START_NUM, LAST_NUM)
     math_operations = ['+', '-', '*']
     operation = math_operations[randint(0, 2)]
-    num = str(num1) + ' ' + str(operation) + ' ' + str(num2)
+    task = str(num1) + ' ' + str(operation) + ' ' + str(num2)
     if operation == '+':
         correct_answer = num1 + num2
     elif operation == '-':
         correct_answer = num1 - num2
     elif operation == '*':
         correct_answer = num1 * num2
-    return num, str(correct_answer)
+    return task, str(correct_answer)
