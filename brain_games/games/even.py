95d1565bfbde4c4090567b38048292685795b53b
irakuruss <132392842+irakuruss@users.noreply.github.com>
2023-06-20 13:15:57 +0300

    Update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,16 +4,16 @@ from random import randint
 GAME_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def determine_num_and_answer():
+def determine_task_and_answer():
     START_NUMBER = 1
     LAST_NUMBER = 1000
-    num = randint(START_NUMBER, LAST_NUMBER)
-    correct_answer = 'yes' if is_even(num) else 'no'
-    return num, correct_answer
+    task = randint(START_NUMBER, LAST_NUMBER)
+    correct_answer = 'yes' if is_even(task) else 'no'
+    return task, correct_answer
 
 
-def is_even(num):
-    if num % 2 == 0:
+def is_even(task):
+    if task % 2 == 0:
         return True
     else:
         return False
