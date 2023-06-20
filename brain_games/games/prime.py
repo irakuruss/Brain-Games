8d7f223b72c12bcecd3fb9402e762b39aa971580
irakuruss <132392842+irakuruss@users.noreply.github.com>
2023-06-20 13:17:45 +0300

    Update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,23 +4,23 @@ from random import randint
 GAME_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def determine_num_and_answer():
+def determine_task_and_answer():
     START_NUM = 1
     LAST_NUM = 1000
-    num = randint(START_NUM, LAST_NUM)
-    correct_answer = 'yes' if is_prime(num) else 'no'
+    task = randint(START_NUM, LAST_NUM)
+    correct_answer = 'yes' if is_prime(task) else 'no'
 
-    return num, correct_answer
+    return task, correct_answer
 
 
-def is_prime(num):
-    if num < 2:
+def is_prime(task):
+    if task < 2:
         return False
 
     divider = 1
 
-    for i in range(1, (num // 2 + 1)):
-        if num % i == 0:
+    for i in range(1, (task // 2 + 1)):
+        if task % i == 0:
             divider += 1
 
     if divider > 2:
