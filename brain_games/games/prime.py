89e10e58584b22ca692e5f855135335b583c9fe9
irakuruss <132392842+irakuruss@users.noreply.github.com>
2023-06-20 13:01:22 +0300

    Update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-game_rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+GAME_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def determine_num_and_answer():
