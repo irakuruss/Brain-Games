84fa58cb3f8d619d29e4526706963da309ca5d7a
irakuruss <132392842+irakuruss@users.noreply.github.com>
2023-07-11 16:37:15 +0300

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -8,17 +8,17 @@ MIN_LENGTH = 5
 MAX_LENGTH = 10
 
 
-def determine_progressions_terms():
-    difference = randint(START_NUM, LAST_NUM)
-    initial_term = randint(START_NUM, LAST_NUM)
-    progression_length = randint(MIN_LENGTH, MAX_LENGTH)
+def determine_progressions_terms(start_num, last_num, min_length, max_length):
+    difference = randint(start_num, last_num)
+    initial_term = randint(start_num, last_num)
+    progression_length = randint(min_length, max_length)
     progression = list(range(initial_term, progression_length * difference
                              + initial_term, difference))
     return progression
 
 
 def determine_task_and_answer():
-    progression = determine_progressions_terms()
+    progression = determine_progressions_terms(START_NUM, LAST_NUM, MIN_LENGTH, MAX_LENGTH)
     correct_answer = choice(progression)
     task = ' '.join(
         '..' if number == correct_answer else str(number)
